03a00e800940df2f57767e366fbcc7c08e90694b
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-09-26 20:16:39 +0000

    Matchy

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -14,9 +14,13 @@
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//create a variable named animal that is {} empty obj
 var animal = {};
+//create key called species that has a string value
 animal.species = "elephant";
+//create key called name that has a string value
 animal["name"] = "Polka";
+//create a key called noises that is [] empty array
 animal.noises = [];
 console.log(animal);
 
@@ -24,12 +28,16 @@ console.log(animal);
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//create noises var that is []
 var noises = [];
+//add 4 elements to noises using various methods
 noises[0] = "stomp";
 noises.push("trumpet");
 noises.unshift("roar");
 noises[noises.length] = "chew";
+//check noises length 
 console.log(noises.length);
+//log the last value in noises
 console.log(noises.length - 1);
 console.log(noises);
 
@@ -37,7 +45,9 @@ console.log(noises);
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//add above noises variable as the key in the animal obj
 animal["noises"] = noises;
+//add another element to the noises array
 noises.push("gulp");
 console.log(animal);
 
@@ -64,12 +74,16 @@ console.log(animal);
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//create new var animals that is []
 var animals = [];
+//push value of animal(which is obj) into the new animals array
 animals.push(animal);
 console.log(animals);
+//create 3 new animals with species, name, and noises 
 var duck = {species: "duck", name: "Jerome", noises: ["quack", "honk", "sneeze", "woosh"]};
 var tiger = {species: "tiger", name: "Lester", noises: ["roar", "purr", "scratch", "jump"]};
 var bird = {species: "bird", name: "Chloe", noises: ["tweet", "singing", "flap", "shuffle"]};
+//push all three animals into the animals array
 animals.push(duck, tiger, bird);
 console.log(animals);
 console.log(animals.length);
@@ -78,12 +92,19 @@ console.log(animals.length);
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//create friends var that is []
 var friends = [];
+//create function getRandom that takes in an array (can be the animals array)
 function getRandom(array){
+  //using Math.random, return a random INDEX from the length of the array
+  //I used Math.floor to make sure it doesn't go below 0.
   return Math.floor(Math.random() * array.length);
 }
+//push that random animal name into friends
+//animals(accesses the array), getRandom(animals)(accesses the random index), .name(accesses that random animal's name)
 friends.push(animals[getRandom(animals)].name);
 console.log(friends);
+//add friends array to a random friend that you choose
 animals[0]["friends"] = friends;
 console.log(animals);
 
