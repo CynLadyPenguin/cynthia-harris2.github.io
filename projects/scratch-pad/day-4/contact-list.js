e8078a1e9a398f70d98debed91850cfe72c9dce8
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-09-26 21:35:15 +0000

    ScratchPad

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -35,10 +35,11 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
+    //return each key in the obj with its value from the parameter
     return {
-    id: id,
-    nameFirst: nameFirst,
-    nameLast: nameLast
+    id: id, //id's value is id
+    nameFirst: nameFirst, //nameFirst's value is nameFirst
+    nameLast: nameLast  //nameLast's value is nameLast
     }
 } 
 
@@ -54,30 +55,46 @@ function makeContactList() {
         length: function() {
             return contacts.length;
         },
+        //addContact is a function with a contact parameter
         addContact: function(contact){
+            //addContact function should return contacts array with contact value pushed into it
             return contacts.push(contact);
         },
+        //function findContact takes in a fullName param
         findContact: function(fullName){
+            //loop through the contacts array forward
             for(let i = 0; i < contacts.length; i++){
+                //if the current element's nameF and nameL is strictly equal to the input fullName
                 if(contacts[i]["nameFirst"] + " " + contacts[i]["nameLast"] === fullName){
+                    //return the matching element
                     return contacts[i];
                 } else {
+                    //otherwise return undefined
                     return undefined;
                 }
             }
         },
+        //function removeContact takes in a contact as a param
         removeContact: function(contact){
+            //loop forward through the contacts array
             for(let i = 0; i < contacts.length; i++){
+                //if the current element matches the input contact
                 if(contacts[i] === contact){
+                    //remove the current element without affecting the other elements. Splice(element to remove, number of elements to remove)
                     return contacts.splice(i, 1);
                 }
             }
         },
+        //function printAllContactNames has no param
         printAllContactNames: function(){
+            //create an empty array var to hold output
             let arr = [];
+            //loop forward through the contacts array
             for(let i = 0; i < contacts.length; i++){
+                //push the current element's first and last name into the empty array var. Each loop cycle will input the next first and last name
                 arr.push(contacts[i]["nameFirst"] + " " + contacts[i]["nameLast"]);
             } 
+            //return the array with each element joined on a new line. New line is denoted by using \n
             return arr.join("\n");
         }
     }
