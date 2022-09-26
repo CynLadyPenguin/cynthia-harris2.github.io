6a1f3797864a9145c74dbca80cf3a09359c77e3a
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-09-26 21:12:49 +0000

    ScratchPad

--- projects/scratch-pad/day-3/homework/range.js
+++ projects/scratch-pad/day-3/homework/range.js
@@ -23,16 +23,24 @@
  */
 function range(start, end) {
     // YOUR CODE GOES BELOW HERE //
+    //create variable to hold the output in an array
     let emptyArray = [];
+    //if the ending # is larger than the starting
     if(end > start){
+        //loop forward from start to end. This loop stops when the element is less than the ending #
         for(let i = start; i <= end; i++){
+            //push each element from loop into array var
             emptyArray.push(i);
         }
+        //if start is greater than ending num
     } else if(end < start){
+        //loop backwards from start to end. Loop always starts at start #. This loop ends when the element is greater than/equal to the ending num
         for(let j = start; j >= end; j--){
+            //push elements into array var
             emptyArray.push(j);
         }
     }
+    //return our output variable with the elements pushed inside
     return emptyArray;
     // YOUR CODE GOES ABOVE HERE //
 }
