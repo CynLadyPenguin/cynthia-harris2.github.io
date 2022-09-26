0efa1f8caea9d6ecefe0cc1f1b31fada103b4130
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-09-26 21:06:47 +0000

    ScratchPad

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,14 +12,22 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+    //create loop that loops over nums 1-100
     for(let i = 1; i < 101; i++){
+        //if the current num when divided by 15 = 0
         if(i % 15 === 0){
+            //log fizzbuzz
             console.log("FizzBuzz");
+            //if num when divided by just 5 is 0
         } else if(i % 5 === 0){
+            //log buzz
             console.log("Buzz");
+            //if num when divided by just 3 is 0
         } else if(i % 3 === 0){
+            //log fizz
             console.log("Fizz");
         } else {
+            //otherwise log the num
             console.log(i);
         }
     }
